26fee02ad834bc02eae7ce78f0a395013edd70a4
Omer Shlifka <omershlifka@gmail.com>
2022-03-12 18:48:37 +0200

    add reactjs to s3 example

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import FileUploader from './FileUploader';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FileUploader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/FileUploader.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import S3 from 'react-aws-s3';
+import { Buffer } from 'buffer'; // This is due to a bug in the library itself
+window.Buffer = Buffer
+
+const config = {
+    bucketName: 'good-forest-static',
+    dirName: 'imgs',
+    region: 'us-east-2',
+    accessKeyId: process.env.S3_KEY || '',
+    secretAccessKey: process.env.S3_SECRET || ''
+}
+const ReactS3Client = new S3(config);
+
+function FileUploader() {
+    const [picName, setPicName] = useState(null);
+    const [picPath, setPicPath] = useState(null);
+    const [errors, setErrors] = useState(null);
+
+    const upload = (file) => {
+        // here you can get file.name and file.size to add some validations
+        setPicName(file.name)
+
+        ReactS3Client.uploadFile(file).then((res) => {
+            console.log(res)
+            setPicPath(res.location)
+        }).catch((err) => {
+            setErrors(err.message)
+        })
+    }
+
+    return (
+        <div>
+            <input
+                type="file"
+                onChange={(e) => upload(e.target.files[0])}></input>
+            <div>File name: {picName}</div>
+            <div>File path: {picPath}</div>
+            <div>Errors: {errors}</div>
+        </div>
+    );
+}
+
+export default FileUploader;
